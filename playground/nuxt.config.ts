07246a191d5ea2f1c0249e8f4a5266e25f7c06de
Pierre-Michel Brown <pierremichelbrown@gmail.com>
2024-06-27 14:28:18 +0200

    chore: Change dev port from 3000 to 5252

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,5 +1,10 @@
+const DEV_PORT = 5252
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  devServer: {
+    port: Number(process.env.PORT) && DEV_PORT,
+  },
   modules: ['@nuxt/ui', 'nuxt-request-timeline'],
   requestTimeline: {},
 
